0e13a814933b15d52ea71758a74a5ed0ba29b79c
Kenrick Chien <ken.chien@gmail.com>
2014-11-09 09:35:42 -0500

    Sorts lists containing two elements

--- spec/sort/mergesort_spec.rb
+++ spec/sort/mergesort_spec.rb
@@ -1,7 +1,10 @@
 module Sort
   class Mergesort
     def sort(list)
-      list
+      return list if list.length == 1
+
+      #swap
+      [ list[1], list[0] ]
     end
   end
 end
@@ -9,5 +12,9 @@ end
 RSpec.describe Sort::Mergesort do
   it "sorts a list of one element"  do
     expect(Sort::Mergesort.new.sort([3])).to eq([3])
+  end
+
+  it "sorts a list of two elements that are not in order" do
+    expect(Sort::Mergesort.new.sort([2,1])).to eq([1,2])
   end
 end
